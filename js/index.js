fc93ab6c39b255ddff4ab21ffb9f867c4fc78949
refaioussama <42978000+refaioussama@users.noreply.github.com>
2018-09-09 18:18:21 +0100

    Cards

---
+++ js/index.js
@@ -0,0 +1,42 @@
+// Enjoy 💙
+// 📧
+// 🌐
+// 📱
+
+// My Email 👋:
+// refaioussama7@gmail.com.com
+
+/* background 
+
+.c_01 .card_Banner{
+    
+    background-image: url("https://images.unsplash.com/photo-1505909182942-e2f09aee3e89?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5257b32f41a8a970f1b644c88174cee5&auto=format&fit=crop&w=1352&q=80");
+}
+
+.c_02 .card_Banner{
+    
+    background-image: url("https://images.unsplash.com/photo-1535106356142-e3169a8b17d2?ixlib=rb-0.3.5&s=0fce1e3c86e04e07abf7305a6fbfaf01&auto=format&fit=crop&w=668&q=80");
+}
+.c_03 .card_Banner{
+    
+    background-image: url("https://images.unsplash.com/photo-1530075288903-69b220251c3e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=032bc1890fe094136025465c66302731&auto=format&fit=crop&w=1534&q=80");
+}
+*/
+
+/*-------------------------------------------------*/
+
+/* photo profil 
+.c_01 .card_Avatar{
+    
+    background-image: url("https://pbs.twimg.com/profile_images/1026096613071237120/LrWQlGzm_400x400.jpg");
+}
+
+.c_02 .card_Avatar{
+   
+    background-image: url("https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=afb35d2683e102d67bcd70b87b100723&auto=format&fit=crop&w=1500&q=80");
+}
+.c_03 .card_Avatar{
+    
+    background-image: url("https://scontent-mrs1-1.xx.fbcdn.net/v/t1.0-9/34984912_10204767465303638_6879517987250896896_n.jpg?_nc_cat=0&oh=6fb98beb8400ff645616ec3603f28f26&oe=5C2404F5");
+}
+*/
